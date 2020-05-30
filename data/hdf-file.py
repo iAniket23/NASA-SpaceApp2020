27a806a10e37c40d789d0b4af6f0e7a095af91da
guillaume@lepointdevente.com <guillaume@lepointdevente.com>
2020-05-30 14:26:30 -0400

    update hdf reader

--- data/hdf-file.py
+++
@@ -1,8 +0,0 @@
-
-import pandas as pd
-
-hdf = pd.HDFStore('../../desktop/datafile.h5', mode='r')
-
-hdf = hdf.keys()
-
-print(hdf)
---
+++ data/hdf.py
@@ -0,0 +1,26 @@
+""" Python HDF File Reader Canvas"""
+
+# Must install 'pandas' and 'tables' for it to work
+import pandas as pd
+
+# Change the directory to file location
+hdf = pd.read_hdf('sample-datafile.h5', mode='r')
+
+"""
+Other useful panda outputs:
+
+pd.read_table()
+pd.read_csv()
+pd.read_excel()
+pd.ExcelFile.parse()
+pd.read_json()
+
+pd.HDFStore.put()
+pd.HDFStore.get()
+pd.HDFStore.select()
+pd.HDFStore.keys()
+"""
+
+
+if __name__ == '__main__':
+    print(hdf)
