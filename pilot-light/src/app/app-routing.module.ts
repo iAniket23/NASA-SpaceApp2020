f37ad47e8c51a0ef2aaa984d1f70b45da52e5887
guillaume@lepointdevente.com <guillaume@lepointdevente.com>
2020-05-31 22:29:37 -0400

    Big Website Upgrade

--- pilot-light/src/app/app-routing.module.ts
+++ pilot-light/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TimelapseComponent } from './timelapse/timelapse.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'timelapse', component: TimelapseComponent},
-  { path: 'mainpage', component: MainPageComponent}
+  { path: 'mainpage', component: MainPageComponent},
+  { path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
--- pilot-light/src/app/app.module.ts
+++ pilot-light/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
 import { TimelapseComponent } from './timelapse/timelapse.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimelapseComponent,
-    MainPageComponent
+    MainPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
